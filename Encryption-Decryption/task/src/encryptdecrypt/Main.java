11f88d7200d8e46bd20d6ff92639eb67ecf78bc2
BTWitsK <BTWitsK@Gmail.com>
2022-03-25 16:54:11 -0600

    finished stage 2

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,20 @@
 package encryptdecrypt;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder output = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+        int shift = scanner.nextInt();
+
+        for (int i = 0; i < message.length(); i++) {
+            int letter = ALPHABET.indexOf(message.charAt(i));
+            output.append(Character.isSpaceChar(message.charAt(i)) ? " " : letter + shift > 25 ?
+                    ALPHABET.charAt((shift  + letter) % 26) : ALPHABET.charAt(letter + shift));
+        }
+        System.out.println(output);
     }
 }
