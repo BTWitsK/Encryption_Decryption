065fd41f76e6320ee434f7be7d3b790b73ac9bd1
BTWitsK <BTWitsK@Gmail.com>
2022-03-27 18:01:11 -0600

    implemented abstract class encryptor

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,17 +1,6 @@
 package encryptdecrypt;
-import java.io.*;
-import java.util.*;
 
 public class Main {
-    static StringBuilder output = new StringBuilder();
-    static HashMap<String, String> argMap = new HashMap<>();
-    static Scanner scanner;
-
-    public static void parseArgs(String[] arg) {
-       for (int i = 0; i < arg.length; i += 2) {
-           argMap.put(arg[i], arg[i + 1]);
-       }
-    }
 
     public static void encryptMessage(String message, int shift) {
         for (int i = 0; i < message.length(); i++) {
@@ -25,32 +14,6 @@ public class Main {
             output.append((char) (message.charAt(i) - shift));
         }
         printMessage(output.toString());
-    }
-
-    public static void printMessage(String output) {
-        if (argMap.containsKey("-out")) {
-            try (PrintWriter writer = new PrintWriter(argMap.get("-out"))) {
-                writer.print(output);
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        } else {
-            System.out.println(output);
-        }
-    }
-
-    public static String parseMessage() {
-       if (argMap.containsKey("-data")) {
-           return argMap.get("-data");
-       } else {
-           try {
-               scanner = new Scanner(new FileReader(argMap.get("-in")));
-               return scanner.nextLine();
-           } catch (IOException e) {
-               System.out.println("Error: No input file found");
-           }
-       }
-       return "";
     }
 
     public static void main(String[] args) {
